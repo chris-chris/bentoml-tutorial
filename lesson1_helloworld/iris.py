fa2e55a1bd039ce28f617dc8ef5aec8bafb48e55
Chris Song <chris@Chrisui-MacBookPro.local>
2021-04-25 17:59:35 +0900

    lesson1

---
+++ lesson1_helloworld/iris.py
@@ -0,0 +1,10 @@
+from sklearn import svm
+from sklearn import datasets
+
+# Load training data
+iris = datasets.load_iris()
+X, y = iris.data, iris.target
+
+# Model Training
+clf = svm.SVC(gamma='scale')
+clf.fit(X, y)
---
+++ lesson1_helloworld/iris_service.py
@@ -0,0 +1,21 @@
+# import the IrisClassifier class defined above
+from iris_classifier import IrisClassifier
+from sklearn import svm
+from sklearn import datasets
+
+# Load training data
+iris = datasets.load_iris()
+X, y = iris.data, iris.target
+
+# Model Training
+clf = svm.SVC(gamma='scale')
+clf.fit(X, y)
+
+# Create a iris classifier service instance
+iris_classifier_service = IrisClassifier()
+
+# Pack the newly trained model artifact
+iris_classifier_service.pack('model', clf)
+
+# Save the prediction service to disk for model serving
+saved_path = iris_classifier_service.save()
