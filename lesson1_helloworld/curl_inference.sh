a726298d29cca3a1b58253bc1db1b5f5a4e068f2
Chris Song <chris@Chrisui-MacBookPro.local>
2021-04-25 20:28:23 +0900

    inference examples

---
+++ lesson1_helloworld/curl_inference.sh
@@ -0,0 +1,5 @@
+curl -i \
+  --header "Content-Type: application/json" \
+  --request POST \
+  --data '[[5.1, 3.5, 1.4, 0.2]]' \
+  http://localhost:5000/predict
---
+++ lesson1_helloworld/launch_iris.sh
@@ -0,0 +1,1 @@
+bentoml serve IrisClassifier:latest
